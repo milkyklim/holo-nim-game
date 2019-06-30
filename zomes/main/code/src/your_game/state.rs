774a7006f675fd1bd166fda9b1471edec4d7dfc8
milkyklim <klim.kolyvanov@gmail.com>
2019-06-30 13:34:09 +0200

    define initial and evolve functions

--- zomes/main/code/src/your_game/state.rs
+++ zomes/main/code/src/your_game/state.rs
@@ -4,6 +4,7 @@ use hdk::holochain_json_api::{
 
 use crate::game_move::Move;
 use crate::game::Game;
+use super::MoveType;
 
 
 /**
@@ -20,16 +21,30 @@ use crate::game::Game;
 #[derive(Clone, Debug, Serialize, Deserialize, DefaultJson)]
 pub struct GameState {
     // <<DEVCAMP-TODO>>
-    // pub moves: Vec<Move>,
+    pub moves: Vec<Move>,
     // Implement your own game state
     // May be helpful to split this into state for each player
+
+    pub player_1_moves: Vec<Piece>,
+    pub player_2_moves: Vec<Piece>,
+
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, DefaultJson)]
+pub struct Piece {
+    pile: usize,
+    n: usize,
 }
 
 
 impl GameState {
     pub fn initial() -> Self {
         // <<DEVCAMP>> return an initial state of a game
-        Self{}
+        Self{
+            moves: Vec::new(),
+            player_1_moves: Vec::new(),
+            player_2_moves: Vec::new(),
+        }
     }
 
     pub fn render(&self) -> String {
@@ -37,11 +52,33 @@ impl GameState {
         "".to_string()
     }
 
-    pub fn evolve(&self, _game: Game, _next_move: &Move) -> GameState {
+    pub fn evolve(&self, game: Game, next_move: &Move) -> GameState {
         // <<DEVCAMP>>
         // given a current state, a game and a move, compute the next state
         // You can assume all moves are valid
-        self.clone()
+        // self.clone()
+
+        let mut moves = self.moves.clone();
+        let mut player_1_moves = self.player_1_moves.clone();
+        let mut player_2_moves = self.player_2_moves.clone();
+
+        moves.push(next_move.clone());
+
+        match next_move.move_type {
+            MoveType::Place{pile, n} => {
+                if game.player_1 == next_move.author{
+                    player_1_moves.push(Piece{pile, n});
+                } else {
+                    player_2_moves.push(Piece{pile, n});
+                }
+            }
+            GameState {
+                moves, 
+                player_1_moves,
+                player_2_moves
+            }
+        }
+
     }
 
 }
