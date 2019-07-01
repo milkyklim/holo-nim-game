bdf04900e95eb235271c88d1694aa075f68cb49d
milkyklim <klim.kolyvanov@gmail.com>
2019-07-01 18:45:45 +0200

    add tests for matchmaking code

--- test/index.js
+++ test/index.js
@@ -1,15 +1,19 @@
 const path = require('path')
 const tape = require('tape')
 
-const { Diorama, tapeExecutor, backwardCompatibilityMiddleware } = require('@holochain/diorama')
+const {
+  Diorama,
+  tapeExecutor,
+  backwardCompatibilityMiddleware
+} = require('@holochain/diorama')
 
 process.on('unhandledRejection', error => {
   // Will print "unhandledRejection err is not defined"
   console.error('got unhandledRejection:', error);
 });
 
-const dnaPath = path.join(__dirname, "../dist/generic-game.dna.json")
-const dna = Diorama.dna(dnaPath, 'generic-game')
+const dnaPath = path.join(__dirname, "../dist/nim.dna.json")
+const dna = Diorama.dna(dnaPath, 'nim')
 
 const diorama = new Diorama({
   instances: {
@@ -24,6 +28,12 @@ const diorama = new Diorama({
 
 
 // <<DEVCAMP>> Your tests here
+// uncomment one of these
+// require('./tictactoe')(diorama.registerScenario)
+// require('./checkers')(diorama.registerScenario)
+// require('./your_game')(diorama.registerScenario)
 
+// test the matchmaking 
+require('./matchmaking')(diorama.registerScenario)
 
 diorama.run()
